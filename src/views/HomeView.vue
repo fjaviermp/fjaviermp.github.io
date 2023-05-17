a43374f1cbc4c852319b27a455482f46d0d4479a
fjaviermp <fjaviermpinf@gmail.com>
2023-05-17 23:47:55 +0200

    Preloader eliminado por tiempo de carga de imagen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 
-import Preloader from '../components/Preloader.vue'
 import ImgPortada from "../components/ImgPortada.vue"
 import Header from '../components/Header.vue'
 import Portada from '../components/HomeComponents/Portada.vue'
@@ -10,24 +9,9 @@ import Servicios from '../components/HomeComponents/Servicios.vue'
 </script>
 
 <template>
-    <Preloader v-if="loading"></Preloader>
     <ImgPortada></ImgPortada>
     <Header></Header>
     <Portada></Portada>
     <Presentacion></Presentacion>
     <Servicios></Servicios>
 </template>
-
-<script lang="ts">
-    export default {
-    name: "App",
-        data() {
-            return {
-                loading: true
-            }
-        },
-        created() {
-            setTimeout(() => this.loading = false, 500);
-        }
-    }
-</script>
