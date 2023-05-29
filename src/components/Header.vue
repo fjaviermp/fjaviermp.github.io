a6e9104fa5f1c40a887a49a451f3393d6777ec67
fjaviermp <fjaviermpinf@gmail.com>
2023-05-29 15:19:22 +0200

    Secciones mas pequeñas excepto la primera

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -106,7 +106,10 @@ export default {
     color: white !important;
     font-size:x-large
 }
-
+.navbar a:hover{
+    color: var(--primary-color) !important;
+    font-size:x-large
+}
 .custom-toggler .navbar-toggler-icon {
   background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgb(255, 255, 255)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
 }
